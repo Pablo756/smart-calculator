c0d6b61e5e11e54da868c3b336750cbab2972569
Pablo <f:\The Bat!>
2018-02-28 08:59:57 +0300

    smart-calculator

--- src/index.js
+++ src/index.js
@@ -1,27 +1,69 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        this.str = initialValue;
+    }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    add(number) {
+        this.str = this.str + " + " + number;
+        return this;
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    subtract(number) {
+        this.str = this.str + " - " + number;
+        return this;
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    multiply(number) {
+        this.str = this.str + " * " + number;
+        return this;
+    }
 
-  pow(number) {
-    // your implementation
-  }
+    devide(number) {
+        this.str = this.str + " / " + number;
+        return this;
+    }
+
+    pow(number) {
+        this.str = this.str + " ^ " + number;
+        return this;
+    }
+
+    valueOf() {
+        var array = this.str.split(' ');
+        for(let i = array.length - 1; i >= 0; i--){
+            if(array[i] === "^"){
+                let val = Math.pow(array[i-1],array[i+1]);
+                array.splice(i-1, 3, val);
+                i = array.length - 1;
+            }
+        }
+
+        for(var i = 0; i < array.length; i++){
+            if(array[i]==="*"){
+                let val = array[i-1] * array[i+1];
+                array.splice(i-1,3,val);
+                i = 0;
+            } else if (array[i] === "/") {
+                let val=array[i-1] / array[i+1];
+                array.splice(i-1,3,val);
+                i = 0;
+            }
+        }
+
+        for(var i = 0; i < array.length; i++){
+            if(array[i] === "+"){
+                let val = +array[i-1]+ +array[i+1];
+                array.splice(i-1,3,val);
+                i=0;
+            } else if(array[i] === "-"){
+                let val = array[i-1] - array[i+1];
+                array.splice(i-1,3,val);
+                i=0;
+            }
+        }
+
+        return array[0];
+    }
 }
 
 module.exports = SmartCalculator;
